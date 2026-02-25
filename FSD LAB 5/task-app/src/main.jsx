4efd8ee3f595fff3707fea0916815a643cc3c108
Alfred <Alfredantony6142@gmailcom>
2026-02-25 11:43:26 +0530

    convert react app into PWA

---
+++ FSD LAB 5/task-app/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(<App />)
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch((err) => console.error('SW registration failed:', err))
+  })
+}
